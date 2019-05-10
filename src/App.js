82fd5702dc09c3d96a0e814b38065da342e66a0d
Job Schipper <job.schipper@medicore.nl>
2019-05-10 17:12:01 +0200

    added working example: publishing a public MAM stream with local PoW

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import * as Mam from 'mam.client.js';
+import * as IOTA from 'iota.lib.js';
+const Curl = require('curl.lib.js');
 
+class App extends React.Component {
+
+    publishMamMessage() {
+
+        var iota = new IOTA({
+            host: 'https://node02.iotatoken.nl',
+            port: 443
+        });
+
+        Curl.init();
+        Curl.overrideAttachToTangle(iota);
+
+        const state = Mam.init(iota);
+        const message = Mam.create(state, 'FOO9BAR9MESSAGE');
+
+        console.log('publishing message to tangle, please hold');
+        console.log('local PoW may take a while');
+
+        Mam.attach(message.payload, message.address);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>MAM publish example</h1>
+                <button onClick={this.publishMamMessage}>Go!</button>
+            </div>
+        );
+
+    }
+}
 export default App;
