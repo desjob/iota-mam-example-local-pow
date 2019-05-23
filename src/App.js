63acf8f1a1b852385dcd3e78cc0f78e88805bc5b
Job Schipper <job.schipper@medicore.nl>
2019-05-23 14:36:51 +0200

    updated to the most recent version of MAM

--- src/App.js
+++ src/App.js
@@ -1,39 +1,107 @@
 import React from 'react';
 import './App.css';
 
-import * as Mam from 'mam.client.js';
-import * as IOTA from 'iota.lib.js';
-const Curl = require('curl.lib.js');
+import * as Converter from '@iota/converter';
+import * as Mam from '@iota/mam';
+import curlTransaction from 'curl-transaction-core';
+import curlImpl from 'curl-transaction-webgl2-impl';
+
+
+
+const curl = curlTransaction({ curlImpl });
+const localAttachToTangle = async function(trunkTransaction, branchTransaction, minWeightMagnitude, trytesArray) {
+
+    var trytes = await curl.curl({ trunkTransaction, branchTransaction, minWeightMagnitude, trytesArray }).then((processedTrytes) => {
+        return processedTrytes
+    }).catch((error) => {
+        throw error;
+    });
+
+    return trytes;
+};
+
 
 class App extends React.Component {
 
+    constructor() {
+
+        super();
+
+        this.state = {
+            status: 'ready',
+            status2: 'ready',
+            root: '',
+            message: 'test 1234'
+        }
+    }
+
+
     publishMamMessage() {
 
-        var iota = new IOTA({
-            host: 'https://node02.iotatoken.nl',
-            port: 443
-        });
+        this.setState({status: 'working'});
 
-        Curl.init();
-        Curl.overrideAttachToTangle(iota);
+        const mamConfig = {
+            provider: "https://nodes.devnet.iota.org:443",
+            attachToTangle: localAttachToTangle
+        };
 
-        const state = Mam.init(iota);
-        const message = Mam.create(state, 'FOO9BAR9MESSAGE');
+        const mamState = Mam.init(mamConfig);
+        const message = Mam.create(mamState, Converter.asciiToTrytes(this.state.message));
 
-        console.log('publishing message to tangle, please hold');
-        console.log('local PoW may take a while');
+        Mam.attach(message.payload, message.address, 3, 9)
+            .then(res => this.messageIsPublished(res, message))
+            .catch(err => console.log(err));
 
-        Mam.attach(message.payload, message.address);
+    }
+
+    messageIsPublished(bundle, message) {
+
+        this.setState({status: JSON.stringify(bundle), root: message.root});
+    }
+
+    receiveMessage() {
+
+        this.setState({status2: 'working'});
+
+        const mamConfig = {
+            provider: "https://nodes.devnet.iota.org:443",
+            attachToTangle: localAttachToTangle
+        };
+
+        const mamState = Mam.init(mamConfig);
+
+        Mam.fetch(this.state.root, 'public')
+            .then(res => this.setState({status2: 'recieved: ' + Converter.trytesToAscii(res.messages[0])}))
+            .catch(err => console.log(err));
+
+    }
+
+    setRoot(root) {
+
+        this.setState({root: root});
+    }
+
+    setMessage(message) {
+
+        this.setState({message: message});
     }
 
     render() {
         return (
             <div>
-                <h1>MAM publish example</h1>
-                <button onClick={this.publishMamMessage}>Go!</button>
+                <h1>IOTA Masked Authenticated Messaging example (using local WebGL PoW)</h1>
+                <input type="text" onChange={this.setMessage.bind(this)} value={this.state.message}/>
+                <button onClick={this.publishMamMessage.bind(this)}>Publish!</button>
+                <p>status: <br/>{this.state.status}</p>
+
+                <hr/>
+                <input type="text" onChange={this.setRoot.bind(this)} value={this.state.root}/>
+                <button onClick={this.receiveMessage.bind(this)}>Receive!</button>
+                <p>status: <br/>{this.state.status2}</p>
             </div>
         );
 
     }
 }
+
 export default App;
