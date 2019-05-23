20b20e8dbdbfe4ea0a42f4e0e175fd97a54ac68d
Job Schipper <job.schipper@medicore.nl>
2019-05-23 14:59:00 +0200

    Updates

---
+++ precache-manifest.f7d27dc06f4cd01d955748313680ae52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c7d94ac04cd48b18ed4ffea7905f28",
+    "url": "/iota-mam-example-local-pow/index.html"
+  },
+  {
+    "revision": "5556d29db1e0742b1dc7",
+    "url": "/iota-mam-example-local-pow/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4e342d78e58c1e783843",
+    "url": "/iota-mam-example-local-pow/static/js/2.21e043ab.chunk.js"
+  },
+  {
+    "revision": "5556d29db1e0742b1dc7",
+    "url": "/iota-mam-example-local-pow/static/js/main.858f86e9.chunk.js"
+  },
+  {
+    "revision": "38228d1a41fa89a765dc",
+    "url": "/iota-mam-example-local-pow/static/js/runtime~main.6b25b2a7.js"
+  }
+]);
